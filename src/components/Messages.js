86c990b2ba146e36ad1f4bd6b26424ca50b8749a
lvemavarapu <lvemavarapu@gmail.com>
2021-10-16 09:35:45 +1000

    Update Messages.js

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import Message from './Message'
 
-const Messages = ({messageList}) =>{
-    
+const Messages =({messageList})=>{
+    console.log(messageList)
     return(
         <div>
             <h3>Messages</h3>
-            {messageList.map((item, index) =>
-                <Message msg={item}/>
+            {messageList.map((message, index)=>
+                <Message key={index} message={message}/>
             )}
         </div>
     )
 }
 
 export default Messages
+
+// import React from 'react'
+// import Message from './Message'
+
+// const Messages = ({messageList}) =>{
+    
+//     return(
+//         <div>
+//             <h3>Messages</h3>
+//             {messageList.map((item, index) =>
+//                 <Message msg={item}/>
+//             )}
+//         </div>
+//     )
+// }
+
+// export default Messages
